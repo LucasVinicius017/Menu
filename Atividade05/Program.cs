51202ed909e209bb8ad0798b4a26ada154645787
Lucas Vinicius dos Santos <lucassantosvini17@gmail.com>
2026-03-02 16:27:26 -0300

    .

--- Atividade05/Program.cs
+++ Atividade05/Program.cs
@@ -10,29 +10,60 @@ namespace Atividade05
     {
         static void Main(string[] args)
         {
+            string nome = "";
+            double nota1 = 0, nota2 = 0, media = 0;
+
+
             string resposta = "";
-            while (resposta != "S")
+            while (resposta.ToUpper() != "S")
             {
                 Console.Clear();
-                Console.Write("1 - Cadastro aluno ");
-                Console.Write("2 - Informar notas ");
-                Console.Write("3 - Calcular médias ");
-                Console.Write("4 - Exibir resultados ");
-                Console.Write("S - Sair ");
+                Console.WriteLine("1 - Cadastro aluno ");
+                Console.WriteLine("2 - Informar notas ");
+                Console.WriteLine("3 - Calcular médias ");
+                Console.WriteLine("4 - Exibir resultados ");
+                Console.WriteLine("S - Sair ");
                 Console.Write(" Escolher a Opção: ");
                 resposta = Console.ReadLine();
 
                 if (resposta == "1")
                 {
+                    Console.Clear();
                     Console.Write("Digite o nome do aluno ");
-                    string nome = Console.ReadLine();
+                    nome = Console.ReadLine();
+                    Console.WriteLine("Aluno cadastrado com sucesso");
                 }
                 else if (resposta == "2")
                 {
+                    Console.Clear();
                     Console.Write("Digite a primeira nota ");
-                    double nota1 = Convert.ToDouble(Console.ReadLine());
+                    nota1 = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Digite a segunda nota ");
+                    nota2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Notas cadastradas com sucesso!");
                 }
+                else if (resposta == "3")
+                {
+                    media = (nota1 + nota2) / 2;
+                    Console.WriteLine("Media calculada!! ");
+                    Console.ReadKey();
+                    Console.Clear();
 
+                }
+                else if (resposta == "4")
+                {
+                    Console.WriteLine("Aluno: " + nome);
+                    Console.WriteLine("Média: " + media);
+
+                    if (media >= 5)
+                    {
+                        Console.WriteLine("Aluno aprovado");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Aluno reprovado");
+                    }
+                    Console.ReadKey();
 
 
             }
